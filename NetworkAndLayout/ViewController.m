3d2e1c8c55a46b0cc2ffd6edbf468a5f1042f9ef
Jam <jiamiao@mayiy.com>
2016-05-11 17:20:34 +0800

    使用TCP模拟一个http报文请求，返回正确数据

--- NetworkAndLayout/ViewController.m
+++ NetworkAndLayout/ViewController.m
@@ -10,7 +10,9 @@
 #import "Masonry.h"
 #import <NetworkAndLayout-swift.h>
 
-@interface ViewController ()
+#import "AsyncSocket.h"
+
+@interface ViewController () <AsyncSocketDelegate>
 
 @property (nonatomic, strong) ColorView *colorView;
 
@@ -26,6 +28,12 @@
     [self.colorView setTranslatesAutoresizingMaskIntoConstraints:NO];
     
     [self.view addSubview:self.colorView];
+    
+    AsyncSocket *socket=[[AsyncSocket alloc] initWithDelegate:self];
+    [socket connectToHost:@"www.mayiw.com" onPort:80 error:nil];
+    
+    [socket readDataWithTimeout:3 tag:1];
+    [socket writeData:[@"GET /suzhou/index.html HTTP/1.1\nHost:www.mayiw.com\n\n" dataUsingEncoding:NSUTF8StringEncoding] withTimeout:3 tag:1];
 }
 
 - (void)viewDidLayoutSubviews {
@@ -48,5 +56,16 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark AsyncSocketDelegate
+
+- (void)onSocket:(AsyncSocket *)sock didConnectToHost:(NSString *)host port:(UInt16)port{
+    NSLog(@"did connect to host");
+}
+
+- (void)onSocket:(AsyncSocket *)sock didReadData:(NSData *)data withTag:(long)tag{
+    NSLog(@"did read data");
+    NSString* message = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    NSLog(@"message is: \n%@",message);
+}
 
 @end
